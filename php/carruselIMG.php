24b200d8cf314b6e112a5766b2ac268fefa007bd
raimundoGarcia <raigarva@gmail.com>
2017-12-17 20:57:23 +0100

    FEAT: Carga del carrusel desde la Base de datos

---
+++ php/carruselIMG.php
@@ -0,0 +1,19 @@
+
+
+<?php
+
+$bd = "gosport";
+$conection = new mysqli("localhost", "root", "root", $bd);
+mysqli_set_charset($conection, "utf8");
+
+if ($conection) {
+   
+    $imgCarrusel = [];
+   
+    $queryCiudades = $conection->query("SELECT * FROM imagenes where descripcion != '' ;");
+    while ($row =$queryCiudades->fetch_array(MYSQLI_ASSOC)) {
+        array_push($imgCarrusel, $row);
+}}
+    
+     echo json_encode($imgCarrusel);
+
