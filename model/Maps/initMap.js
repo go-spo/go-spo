61c6a1f379a278d5fc8eec906590d8383f62a10b
Daniel Alba Diaz <danialba96@gmail.com>
2018-01-10 12:46:03 +0100

    effect added

--- model/Maps/initMap.js
+++ model/Maps/initMap.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     var markadores = [];
     var centrado = {lat: 39.478758, lng: -0.414405};
-
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
         center: centrado
@@ -12,12 +11,30 @@ $(document).ready(function () {
         url: '../model/Maps/initMap.php',
         dataType: 'json',
         success: function (markers) {
-            console.log("hola");
+
             markers.forEach(n => {
                 var posicion = new google.maps.LatLng(n.coordenada_x, n.coordenada_y);
+                var infowindow = new google.maps.InfoWindow({
+                    content: n.nombre
+                });
                 var marker = new google.maps.Marker({
                     position: posicion,
-                    map: map
+                    map: map,
+                });
+                marker.addListener('click', function () {
+                    infowindow.open(map, marker);
+                    var lat = this.getPosition().lat();
+                    var lng = this.getPosition().lng();
+                    ///////fumada ajax anidado
+                    $.ajax({
+                        url: '../model/Maps/sideMaps/datos_seleccionados.php',
+                        dataType: 'json',
+                        data: {latitud: lat, longitud: lng},
+                        success: function (seleccionado) {
+                            console.log("funciona!");
+                        }
+                    });
+                    //////
                 });
             });
         }
